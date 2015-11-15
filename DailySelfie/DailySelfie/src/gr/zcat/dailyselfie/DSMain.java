286b9a7b48441d9d8a60ceb8a41b28ecacfc60e0
Zeus Casper <zeus3droid@gmail.com>
2015-11-15 21:38:48 +0200

    lab 8 4/4 peer wip

---
+++ DailySelfie/DailySelfie/src/gr/zcat/dailyselfie/DSMain.java
@@ -0,0 +1,36 @@
+package gr.zcat.dailyselfie;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class DSMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dsmain);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.dsmain, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.snap) {
+			Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
